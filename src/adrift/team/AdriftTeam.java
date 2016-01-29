601e33e945bbc47e02517f67d9312eb694eec0d6
Philip <Philip@10.1.0.15>
2016-01-28 21:14:31 -0600

    1/28/2016 - Initial commit to repository

---
+++ src/adrift/team/AdriftTeam.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adrift.team;
+
+/**
+ *
+ * @author Philip
+ */
+public class AdriftTeam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //TestRyansModules();
+        TestPhilipsModules();
+
+        // Player - Team
+        // Game - Ryan
+        // Ship - Ryan
+        // Robot - Ryan
+        // InventoryItem - Ryan
+        // Actor - Ryan
+        // Map - Ryan
+        // Buggy - Philip
+        // Space Suite - Philip
+        // Location - Philip
+        // ResourceScene - Philip
+        // RegularScene - Philip
+        // NASABase - Philip
+        // Outpost - Philip
+    }
+
+    public static void TestRyansModules() {
+        // Ryan puts his test code in here.
+    }
+
+    public static void TestPhilipsModules() {
+    }
+
+}
