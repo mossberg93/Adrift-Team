b51465ee88903743eccfd405a98341c6a08a1eaf
Philip <Philip@10.1.0.15>
2016-02-10 18:36:27 -0600

    2/10/2016 - Group control classes and unit test 2/10/2016 - Philip unit test

---
+++ src/byui/cit260/adtift/control/RobotControl.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.adtift.control;
+
+/**
+ *
+ * @author Philip
+ */
+public class RobotControl {
+    
+    public double calculateO2(int distance, int amount, int harvest, int buggy) {
+        
+        if (distance < 0) {
+            return -1;
+        }
+        
+        if (amount < 0) {
+            return -1;
+        }
+        
+        if (harvest != 0 && harvest != 1) {
+            return -1;
+        }
+        
+        if (buggy != 0 && buggy != 1) {
+            return -1;
+        }
+        
+        double totalO2 = distance + harvest + (amount * distance) - (amount * buggy);
+        
+        return totalO2;
+    }
+
+    public double calculateEnergy(int distance, int amount, int harvest, int buggy) {
+        
+        if (distance < 0) {
+            return -1;
+        }
+        
+        if (amount < 0) {
+            return -1;
+        }
+        
+        if (harvest != 0 && harvest != 1) {
+            return -1;
+        }
+        
+        if (buggy != 0 && buggy != 1) {
+            return -1;
+        }
+        
+        double totalEnergy = distance + harvest + (amount * distance) - (amount * buggy);
+        
+        return totalEnergy;
+    }
+}
---
+++ src/byui/cit260/adtift/control/SpaceSuitControl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.adtift.control;
+
+/**
+ *
+ * @author Philip
+ */
+public class SpaceSuitControl {
+    
+}
